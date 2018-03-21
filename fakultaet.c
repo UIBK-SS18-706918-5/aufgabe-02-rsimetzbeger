ae3ac561d0392501a5d56764717a43b7c9e027fc
rsimetzbeger <Robert.Simetzberger@student.uibk.ac.at>
2018-03-21 16:38:50 +0100

    Fakultaet berechnen

---
+++ fakultaet.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>	// Neue Bibliothek - benötigt für atof
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+	int f, i ;
+	int s = 1;
+	if (argc == 2) {
+
+	// Einlesen der Variable
+f = atof(argv[1]);}
+
+	// Fakultät berechnen
+	for (i = 1;i <= f; i++) { 
+		s = i*s;}
+	//Ausgabe
+	printf("Fakultaet von %i ist %i \n",f,s);
+return 0;
+}
+
